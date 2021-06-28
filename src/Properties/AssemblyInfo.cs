a154b18f2b40e9beb40122050084ade51bce3617
dmester <daniel@mester.se>
2021-06-28 16:55:04 +0200

    Enabled strong name signing.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,4 +4,9 @@
 
 using System.Runtime.CompilerServices;
 
+#if SIGNED_ASSEMBLIES
+[assembly: InternalsVisibleTo("PdfToSvg.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010061da6a0cd2c597cfa715f18b3d8fd236eb8fa0f35e361777a94c832c9752612fe0350913a95cb36f633d853dfffb3cf662c48e110ab9b808158c27d72d5e2d6d9aa5b7016cb12e48faa278214e382f5936f04c79ca012ec32ef5194d9db01397d8a64a36441bb2ee8657fc62a9e4d92bd9fd134d5a2ebdc48c2c973a5788faa8")]
+#else
 [assembly: InternalsVisibleTo("PdfToSvg.Tests")]
+#endif
+

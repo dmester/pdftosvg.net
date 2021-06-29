300d4528f37788a68cb6ccdac5d45d73557f2d2c
dmester <daniel@mester.se>
2021-06-29 13:30:05 +0200

    `StableID` now supports nested `IEnumerable` and `Stream` arguments.

--- src/Common/StableId.cs
+++ src/Common/StableId.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -15,54 +16,25 @@ namespace PdfToSvg.Common
 {
     internal static class StableID
     {
-        public static string Generate(string prefix, IEnumerable inputs)
-        {
-            var sb = new StringBuilder();
+        private const int BufferSize = 1024;
+        private const int IdLength = 7;
 
-            if (inputs != null)
-            {
-                foreach (var input in inputs)
-                {
-                    if (input != null)
-                    {
-                        if (input is double inputDbl)
-                        {
-                            // This special case is to ensure the same id is generated on .NET Core and Framework.
-                            // See https://devblogs.microsoft.com/dotnet/floating-point-parsing-and-formatting-improvements-in-net-core-3-0/
-                            sb.Append(inputDbl.ToString("G15", CultureInfo.InvariantCulture));
-                        }
-                        else if (input is IFormattable formattable)
-                        {
-                            sb.Append(formattable.ToString(null, CultureInfo.InvariantCulture));
-                        }
-                        else
-                        {
-                            sb.Append(input.ToString());
-                        }
-                    }
+        private static readonly byte[] Separator = new byte[] { 0, 0 };
 
-                    sb.Append('\0');
-                }
-            }
-
-            return Generate(prefix, sb.ToString());
-        }
-
-        public static string Generate(string prefix, params object?[] inputs)
-        {
-            return Generate(prefix, (IEnumerable)inputs);
-        }
-
-        public static string Generate(string prefix, string input)
+        public static string Generate(string prefix, object inputs)
         {
             using (var sha1 = SHA1.Create())
             {
                 const string Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-                var inputBytes = Encoding.Unicode.GetBytes(input);
-                var hashBytes = sha1.ComputeHash(inputBytes);
+                var buffer = new byte[BufferSize];
+                Update(sha1, inputs, buffer);
 
-                var result = new char[7];
+                sha1.TransformFinalBlock(ArrayUtils.Empty<byte>(), 0, 0);
+
+                var hashBytes = sha1.Hash;
+
+                var result = new char[IdLength];
                 prefix.CopyTo(0, result, 0, prefix.Length);
 
                 for (var i = prefix.Length; i < result.Length; i++)
@@ -71,6 +43,79 @@ namespace PdfToSvg.Common
                 }
 
                 return new string(result);
+            }
+        }
+
+        public static string Generate(string prefix, params object?[] inputs)
+        {
+            return Generate(prefix, (object)inputs);
+        }
+
+        private static void Update(HashAlgorithm hashAlgorithm, object? input, byte[] buffer)
+        {
+            if (input != null)
+            {
+                string? sValue = null;
+
+                if (input is string tmpStringValue)
+                {
+                    // Must be checked before IEnumerable, since string implements IEnumerable
+                    sValue = tmpStringValue;
+                }
+                else if (input is byte[] byteArray)
+                {
+                    hashAlgorithm.TransformBlock(byteArray, 0, byteArray.Length, null, 0);
+                }
+                else if (input is IEnumerable enumerable)
+                {
+                    foreach (var item in enumerable)
+                    {
+                        Update(hashAlgorithm, item, buffer);
+                        hashAlgorithm.TransformBlock(Separator, 0, Separator.Length, null, 0);
+                    }
+                }
+                else if (input is double inputDbl)
+                {
+                    // This special case is to ensure the same id is generated on .NET Core and Framework.
+                    // See https://devblogs.microsoft.com/dotnet/floating-point-parsing-and-formatting-improvements-in-net-core-3-0/
+                    sValue = inputDbl.ToString("G15", CultureInfo.InvariantCulture);
+                }
+                else if (input is IFormattable formattable)
+                {
+                    sValue = formattable.ToString(null, CultureInfo.InvariantCulture);
+                }
+                else if (input is Stream stream)
+                {
+                    int read;
+
+                    do
+                    {
+                        read = stream.Read(buffer, 0, buffer.Length);
+                        hashAlgorithm.TransformBlock(buffer, 0, read, null, 0);
+                    }
+                    while (read > 0);
+                }
+                else
+                {
+                    sValue = input.ToString();
+                }
+
+                if (sValue != null)
+                {
+                    var encoding = new UnicodeEncoding(false, false);
+                    //var encoding = Encoding.Unicode;
+                    var maxCharsPerIteration = buffer.Length >> 1;
+
+                    for (var stringCursor = 0; stringCursor < sValue.Length;)
+                    {
+                        var charCount = Math.Min(sValue.Length - stringCursor, maxCharsPerIteration);
+                        var bytesWritten = encoding.GetBytes(sValue, stringCursor, charCount, buffer, 0);
+
+                        hashAlgorithm.TransformBlock(buffer, 0, bytesWritten, null, 0);
+
+                        stringCursor += charCount;
+                    }
+                }
             }
         }
     }
---
+++ tests/PdfToSvg.Tests/Common/StableIDTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) PdfToSvg.NET contributors.
+// https://github.com/dmester/pdftosvg.net
+// Licensed under the MIT License.
+
+using NUnit.Framework;
+using PdfToSvg.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfToSvg.Tests.Common
+{
+    public class StableIDTests
+    {
+        [Test]
+        public void Numbers()
+        {
+            Assert.AreEqual(StableID.Generate("im", 1, 11, 3), StableID.Generate("im", "1", "11", "3"));
+            Assert.AreNotEqual(StableID.Generate("im", 11, 1, 3), StableID.Generate("im", "1", "11", "3"));
+        }
+
+        [Test]
+        public void Null()
+        {
+            Assert.AreEqual(StableID.Generate("im", null), StableID.Generate("im", null));
+            Assert.AreEqual(StableID.Generate("im", new object[] { null, null }), StableID.Generate("im", new object[] { null, null }));
+
+            Assert.AreNotEqual(StableID.Generate("im", 0), StableID.Generate("im", null));
+            Assert.AreNotEqual(StableID.Generate("im", new object[] { null, null, null }), StableID.Generate("im", new object[] { null, null }));
+            Assert.AreNotEqual(StableID.Generate("im", new object[] { null, null, 0 }), StableID.Generate("im", new object[] { null, null }));
+        }
+
+        [Test]
+        public void Enumerables()
+        {
+            Assert.AreEqual(StableID.Generate("im", new int[] { 1, 2, 3 }), StableID.Generate("im", new int[] { 1, 2, 3 }));
+            Assert.AreNotEqual(StableID.Generate("im", new int[] { 1, 2, 4 }), StableID.Generate("im", new int[] { 1, 2, 3 }));
+            Assert.AreNotEqual(StableID.Generate("im", new int[] { 1, 2 }), StableID.Generate("im", new int[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void NestedEnumerables()
+        {
+            Assert.AreEqual(
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 2 }, "abc" } }),
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 2 }, "abc" } }));
+
+            Assert.AreNotEqual(
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 2 }, "abc" } }),
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 3 }, "abc" } }));
+
+            Assert.AreNotEqual(
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 2 }, "abc" } }),
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1 }, "abc" } }));
+
+            Assert.AreNotEqual(
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 2 }, "abc" } }),
+                StableID.Generate("im", new object[] { new object[] { new int[] { 1, 3 }, "abC" } }));
+        }
+
+        [Test]
+        public void Stream()
+        {
+            var bytes = new byte[10000];
+            var random = new Random(1);
+            random.NextBytes(bytes);
+
+            var otherBytes = (byte[])bytes.Clone();
+
+            var stream = new MemoryStream(bytes);
+
+            stream.Position = 0;
+            Assert.AreEqual(StableID.Generate("im", otherBytes), StableID.Generate("im", stream));
+
+            stream.Position = 0;
+
+            otherBytes[4215] = unchecked((byte)(otherBytes[4215] - 1));
+            Assert.AreNotEqual(StableID.Generate("im", otherBytes), StableID.Generate("im", stream));
+        }
+    }
+}

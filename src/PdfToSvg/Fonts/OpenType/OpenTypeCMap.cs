9a93bd77584f96ff2b2a73815c7c44b509e9e55a
Daniel Mester Pirttijärvi <dmester@users.noreply.github.com>
2022-09-04 18:24:14 +0200

    Lookup methods in OpenTypeCMap.

--- src/PdfToSvg/Fonts/OpenType/OpenTypeCMap.cs
+++ src/PdfToSvg/Fonts/OpenType/OpenTypeCMap.cs
@@ -15,12 +15,16 @@ namespace PdfToSvg.Fonts.OpenType
     internal class OpenTypeCMap
     {
         private readonly List<OpenTypeCMapRange> rangesByGlyphIndex;
+        private readonly List<OpenTypeCMapRange> rangesByUnicode;
         private ReadOnlyCollection<OpenTypeCMapRange>? readOnlyRanges;
 
         public OpenTypeCMap(OpenTypePlatformID platformID, int encodingID, IEnumerable<OpenTypeCMapRange> ranges)
         {
             rangesByGlyphIndex = ranges.ToList();
             rangesByGlyphIndex.Sort((a, b) => Comparer<uint>.Default.Compare(a.StartGlyphIndex, b.StartGlyphIndex));
+
+            rangesByUnicode = rangesByGlyphIndex.ToList();
+            rangesByUnicode.Sort((a, b) => Comparer<uint>.Default.Compare(a.StartUnicode, b.StartUnicode));
 
             PlatformID = platformID;
             EncodingID = encodingID;
@@ -53,6 +57,49 @@ namespace PdfToSvg.Fonts.OpenType
                     }
                 }
             }
+        }
+
+        public uint? ToGlyphIndex(string unicode)
+        {
+            if (unicode == null) throw new ArgumentNullException(nameof(unicode));
+            if (unicode.Length == 0) throw new ArgumentException("Unicode string cannot be empty.", nameof(unicode));
+
+            var codePoint = Utf16Encoding.DecodeCodePoint(unicode, 0, out var length);
+            if (unicode.Length != length)
+            {
+                // Contains several characters
+                return null;
+            }
+
+            return ToGlyphIndex(codePoint);
+        }
+
+        public uint? ToGlyphIndex(uint unicodeCodePoint)
+        {
+            var min = 0;
+            var max = rangesByUnicode.Count - 1;
+
+            while (min <= max)
+            {
+                var mid = min + ((max - min) >> 1);
+                var range = rangesByUnicode[mid];
+
+                if (unicodeCodePoint < range.StartUnicode)
+                {
+                    max = mid - 1;
+                }
+                else if (unicodeCodePoint > range.EndUnicode)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    var glyphIndex = range.StartGlyphIndex + (unicodeCodePoint - range.StartUnicode);
+                    return glyphIndex;
+                }
+            }
+
+            return null;
         }
 
         public string? ToUnicode(uint glyphIndex)

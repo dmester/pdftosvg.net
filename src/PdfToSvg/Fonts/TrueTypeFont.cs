54866d3488fab5d6a830204c9690b07df9b9ab81
Daniel Mester Pirttijärvi <dmester@users.noreply.github.com>
2024-06-06 18:16:52 +0200

    Improved handling of simple TrueType text encoding

--- src/PdfToSvg/Fonts/TrueTypeFont.cs
+++ src/PdfToSvg/Fonts/TrueTypeFont.cs
@@ -42,27 +42,179 @@ namespace PdfToSvg.Fonts
             return lookup;
         }
 
+        private IEnumerable<CharInfo> GetEncodingChars(SingleByteEncoding encoding)
+        {
+            return Enumerable.Range(0, 256)
+                .Select(charCode => new CharInfo
+                {
+                    CharCode = (uint)charCode,
+                    GlyphName = encoding.GetGlyphName((byte)charCode),
+                    Unicode = encoding.GetUnicode((byte)charCode) ?? CharInfo.NotDef,
+                })
+                .Where(ch => ch.Unicode != null);
+        }
+
+        private IEnumerable<CharInfo> GetSymbolicChars(OpenTypeCMap cmap)
+        {
+            var postGlyphNames = GetGlyphNameLookup();
+            var cmapEncoding = cmap.PlatformID == OpenTypePlatformID.Macintosh ? SingleByteEncoding.MacRoman : null;
+
+            return cmap.Chars
+               .Where(ch =>
+               {
+                   // Allowed ranges according to section 9.6.6.4:
+                   // 0x0000-0x00FF, 0xF000-0xF0FF, 0xF100-0xF1FF, 0xF200-0xF2FF
+
+                   var hibyte = ch.Unicode >> 8;
+
+                   return
+                       hibyte == 0x00 ||
+                       hibyte == 0xF0 ||
+                       hibyte == 0xF1 ||
+                       hibyte == 0xF2;
+               })
+               .Select(ch => {
+
+                   string? unicode = null;
+                   string? glyphName = null;
+
+                   // First hand: glyph names from Post table
+                   if (ch.GlyphIndex < postGlyphNames.Length)
+                   {
+                       glyphName = postGlyphNames[ch.GlyphIndex];
+                       AdobeGlyphList.TryGetUnicode(glyphName, out unicode);
+                   }
+
+                   // Second hand: Mac OS Roman encoding
+                   if (unicode == null && cmapEncoding != null)
+                   {
+                       unicode = cmapEncoding.GetUnicode((byte)ch.Unicode);
+                   }
+
+                   // Third hand:
+                   // Since this is a symbolic encoding, there is no point in trying to give the character codes any meaning.
+                   // There is a risk that the glyphs are then mapped to characters with special meaning, like soft hyphen, line break etc.
+                   // Because of this, ensure all characters are mapped to the private use area.
+                   if (unicode == null)
+                   {
+                       if (ch.Unicode >= 0xF000)
+                       {
+                           unicode = Utf16Encoding.EncodeCodePoint(ch.Unicode);
+                       }
+                       else
+                       {
+                           unicode = Utf16Encoding.EncodeCodePoint(0xF000 | (ch.Unicode & 0xff));
+                       }
+                   }
+
+                   return new CharInfo
+                   {
+                       CharCode = ch.Unicode & 0xff,
+                       GlyphIndex = ch.GlyphIndex,
+                       GlyphName = glyphName,
+                       Unicode = unicode ?? CharInfo.NotDef,
+                   };
+               });
+        }
+
+        private IEnumerable<CharInfo> GetNonSymbolicChars(OpenTypeCMap cmap, SingleByteEncoding encoding)
+        {
+            var postGlyphNames = GetGlyphNameLookup();
+
+            IEnumerable<CharInfo?> glyphNames;
+
+            if (cmap.PlatformID == OpenTypePlatformID.Windows)
+            {
+                // Glyph name -> AGL -> Unicode -> CMap -> Glyph index
+                glyphNames = cmap.Chars
+                    .Select(ch =>
+                    {
+                        var unicode = Utf16Encoding.EncodeCodePoint(ch.Unicode);
+
+                        if (unicode != null)
+                        {
+                            if (AdobeGlyphList.TryGetGlyphName(unicode, out var glyphName))
+                            {
+                                return new CharInfo
+                                {
+                                    GlyphIndex = ch.GlyphIndex,
+                                    Unicode = unicode,
+                                    GlyphName = glyphName,
+                                };
+                            }
+                        }
+
+                        return null;
+                    });
+            }
+            else
+            {
+                // Glyph name -> Mac OS Roman -> Char code -> CMap -> Glyph index
+                var macos = SingleByteEncoding.MacRoman;
+
+                glyphNames = cmap.Chars
+                    .Select(ch =>
+                    {
+                        var glyphName = macos.GetGlyphName((byte)ch.Unicode);
+                        var unicode = macos.GetUnicode((byte)ch.Unicode);
+
+                        if (glyphName != null && unicode != null)
+                        {
+                            return new CharInfo
+                            {
+                                GlyphName = glyphName,
+                                GlyphIndex = ch.GlyphIndex,
+                                Unicode = unicode,
+                            };
+                        }
+
+                        return null;
+                    });
+            }
+
+            // Glyph name -> Post table -> Glyph index
+            glyphNames = glyphNames.Concat(postGlyphNames
+                .Select((glyphName, glyphIndex) =>
+                {
+                    AdobeGlyphList.TryGetUnicode(glyphName, out var unicode);
+
+                    return new CharInfo
+                    {
+                        GlyphIndex = (uint)glyphIndex,
+                        Unicode = unicode ?? CharInfo.NotDef,
+                        GlyphName = glyphName,
+                    };
+                }));
+
+            // Char code -> Encoding -> Glyph name
+            var charCodeLookup = Enumerable.Range(0, 256)
+                .Select(charCode => new { GlyphName = encoding.GetGlyphName((byte)charCode), CharCode = charCode })
+                .Where(ch => ch.GlyphName != null)
+                .ToLookup(ch => ch.GlyphName, ch => (uint)ch.CharCode);
+
+            return glyphNames
+                .WhereNotNull()
+                .SelectMany(glyph => charCodeLookup[glyph.GlyphName]
+                    .Select(charCode =>
+                    {
+                        var clone = glyph.Clone();
+                        clone.CharCode = charCode;
+                        return clone;
+                    }));
+        }
+
         protected override IEnumerable<CharInfo> GetChars()
         {
             // ISO 32000-2 section 9.6.5.4
             var cmaps = openTypeFont?.CMaps ?? ArrayUtils.Empty<OpenTypeCMap>();
-            var postGlyphNames = GetGlyphNameLookup();
             var encoding = pdfFontEncoding ?? SingleByteEncoding.Standard;
-
-            var chars = Enumerable.Empty<CharInfo>();
 
             if (openTypeFont == null)
             {
-                chars = Enumerable.Range(0, 256)
-                    .Select(charCode => new CharInfo
-                    {
-                        CharCode = (uint)charCode,
-                        GlyphName = encoding.GetGlyphName((byte)charCode),
-                        Unicode = encoding.GetUnicode((byte)charCode) ?? CharInfo.NotDef,
-                    })
-                    .Where(ch => ch.Unicode != null);
+                return GetEncodingChars(encoding);
             }
-            else if (pdfFontEncoding == null || isSymbolic)
+
+            if (isSymbolic)
             {
                 // Symbolic font
                 var cmap =
@@ -71,124 +223,22 @@ namespace PdfToSvg.Fonts
 
                 if (cmap != null)
                 {
-                    chars = cmap.Chars
-                        .Where(ch =>
-                        {
-                            // Allowed ranges according to section 9.6.6.4:
-                            // 0x0000-0x00FF, 0xF000-0xF0FF, 0xF100-0xF1FF, 0xF200-0xF2FF
-
-                            var hibyte = ch.Unicode >> 8;
-
-                            return
-                                hibyte == 0x00 ||
-                                hibyte == 0xF0 ||
-                                hibyte == 0xF1 ||
-                                hibyte == 0xF2;
-                        })
-                        .Select(ch => new CharInfo
-                        {
-                            CharCode = ch.Unicode & 0xff,
-                            GlyphIndex = ch.GlyphIndex,
-                            Unicode = Utf16Encoding.EncodeCodePoint(ch.Unicode) ?? CharInfo.NotDef,
-                        });
+                    return GetSymbolicChars(cmap);
                 }
             }
             else
             {
-                var glyphNames = Enumerable.Empty<CharInfo?>();
+                var cmap =
+                    cmaps.FirstOrDefault(cmap => cmap.PlatformID == OpenTypePlatformID.Windows && cmap.EncodingID == 1) ??
+                    cmaps.FirstOrDefault(cmap => cmap.PlatformID == OpenTypePlatformID.Macintosh && cmap.EncodingID == 0);
 
-                var cmap31 = cmaps.FirstOrDefault(cmap => cmap.PlatformID == OpenTypePlatformID.Windows && cmap.EncodingID == 1);
-                if (cmap31 != null)
+                if (cmap != null)
                 {
-                    // Glyph name -> AGL -> Unicode -> CMap -> Glyph index
-                    glyphNames = cmap31.Chars
-                        .Select(ch =>
-                        {
-                            var unicode = Utf16Encoding.EncodeCodePoint(ch.Unicode);
-
-                            if (unicode != null)
-                            {
-                                if (AdobeGlyphList.TryGetGlyphName(unicode, out var glyphName))
-                                {
-                                    return new CharInfo
-                                    {
-                                        GlyphIndex = ch.GlyphIndex,
-                                        Unicode = unicode,
-                                        GlyphName = glyphName,
-                                    };
-                                }
-                            }
-
-                            return null;
-                        });
+                    return GetNonSymbolicChars(cmap, encoding);
                 }
-                else
-                {
-                    var cmap10 = cmaps.FirstOrDefault(cmap => cmap.PlatformID == OpenTypePlatformID.Macintosh && cmap.EncodingID == 0);
-                    if (cmap10 != null)
-                    {
-                        // Glyph name -> Mac OS Roman -> Char code -> CMap -> Glyph index
-                        var macos = new MacRomanEncoding();
-
-                        glyphNames = cmap10.Chars
-                           .Select(ch =>
-                           {
-                               var glyphName = macos.GetGlyphName((byte)ch.Unicode);
-                               var unicode = macos.GetUnicode((byte)ch.Unicode);
-
-                               if (glyphName != null && unicode != null)
-                               {
-                                   return new CharInfo
-                                   {
-                                       GlyphName = glyphName,
-                                       GlyphIndex = ch.GlyphIndex,
-                                       Unicode = unicode,
-                                   };
-                               }
-
-                               return null;
-                           });
-                    }
-                }
-
-                // Glyph name -> Post table -> Glyph index
-                glyphNames = glyphNames.Concat(postGlyphNames
-                    .Select((glyphName, glyphIndex) =>
-                    {
-                        AdobeGlyphList.TryGetUnicode(glyphName, out var unicode);
-
-                        return new CharInfo
-                        {
-                            GlyphIndex = (uint)glyphIndex,
-                            Unicode = unicode ?? CharInfo.NotDef,
-                            GlyphName = glyphName,
-                        };
-                    }));
-
-                // Char code -> Encoding -> Glyph name
-                var charCodeLookup = Enumerable.Range(0, 256)
-                    .Select(charCode => new { GlyphName = encoding.GetGlyphName((byte)charCode), CharCode = charCode })
-                    .Where(ch => ch.GlyphName != null)
-                    .ToLookup(ch => ch.GlyphName, ch => (uint)ch.CharCode);
-
-                chars = glyphNames
-                    .WhereNotNull()
-                    .Select(glyph =>
-                    {
-                        var charCode = charCodeLookup[glyph.GlyphName].Cast<uint?>().FirstOrDefault();
-
-                        if (charCode.HasValue)
-                        {
-                            glyph.CharCode = charCode.Value;
-                            return glyph;
-                        }
-
-                        return null;
-                    })
-                    .WhereNotNull();
             }
 
-            return chars;
+            return Enumerable.Empty<CharInfo>();
         }
 
         public override string ToString() => base.ToString() + "; TrueType";

024798f8a66a9e67f98ea59c987b671986344fb1
Daniel Mester Pirttijärvi <dmester@users.noreply.github.com>
2025-03-23 15:12:28 +0100

    Bug fix: char code 312 in MacRomanEncoding and 240 in WinAnsiEncoding were incorrectly coded as no break space

--- src/PdfToSvg/Encodings/MacExpertEncoding.cs
+++ src/PdfToSvg/Encodings/MacExpertEncoding.cs
@@ -18,38 +18,38 @@ namespace PdfToSvg.Encodings
 
         private static readonly string?[] chars = new[]
         {
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            "\u0020", "\uf721", "\uf6f8", "\uf7a2", "\uf724", "\uf6e4", "\uf726", "\uf7b4",
-            "\u207d", "\u207e", "\u2025", "\u2024", "\u002c", "\u002d", "\u002e", "\u2044",
-            "\uf730", "\uf731", "\uf732", "\uf733", "\uf734", "\uf735", "\uf736", "\uf737",
-            "\uf738", "\uf739", "\u003a", "\u003b", nullchar, "\uf6de", nullchar, "\uf73f",
-            nullchar, nullchar, nullchar, nullchar, "\uf7f0", nullchar, nullchar, "\u00bc",
-            "\u00bd", "\u00be", "\u215b", "\u215c", "\u215d", "\u215e", "\u2153", "\u2154",
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, "\ufb00", "\ufb01",
-            "\ufb02", "\ufb03", "\ufb04", "\u208d", nullchar, "\u208e", "\uf6f6", "\uf6e5",
-            "\uf760", "\uf761", "\uf762", "\uf763", "\uf764", "\uf765", "\uf766", "\uf767",
-            "\uf768", "\uf769", "\uf76a", "\uf76b", "\uf76c", "\uf76d", "\uf76e", "\uf76f",
-            "\uf770", "\uf771", "\uf772", "\uf773", "\uf774", "\uf775", "\uf776", "\uf777",
-            "\uf778", "\uf779", "\uf77a", "\u20a1", "\uf6dc", "\uf6dd", "\uf6fe", nullchar,
-            nullchar, "\uf6e9", "\uf6e0", nullchar, nullchar, nullchar, nullchar, "\uf7e1",
-            "\uf7e0", "\uf7e2", "\uf7e4", "\uf7e3", "\uf7e5", "\uf7e7", "\uf7e9", "\uf7e8",
-            "\uf7ea", "\uf7eb", "\uf7ed", "\uf7ec", "\uf7ee", "\uf7ef", "\uf7f1", "\uf7f3",
-            "\uf7f2", "\uf7f4", "\uf7f6", "\uf7f5", "\uf7fa", "\uf7f9", "\uf7fb", "\uf7fc",
-            nullchar, "\u2078", "\u2084", "\u2083", "\u2086", "\u2088", "\u2087", "\uf6fd",
-            nullchar, "\uf6df", "\u2082", nullchar, "\uf7a8", nullchar, "\uf6f5", "\uf6f0",
-            "\u2085", nullchar, "\uf6e1", "\uf6e7", "\uf7fd", nullchar, "\uf6e3", nullchar,
-            nullchar, "\uf7fe", nullchar, "\u2089", "\u2080", "\uf6ff", "\uf7e6", "\uf7f8",
-            "\uf7bf", "\u2081", "\uf6f9", nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, "\uf7b8", nullchar, nullchar, nullchar, nullchar, nullchar, "\uf6fa",
-            "\u2012", "\uf6e6", nullchar, nullchar, nullchar, nullchar, "\uf7a1", nullchar,
-            "\uf7ff", nullchar, "\u00b9", "\u00b2", "\u00b3", "\u2074", "\u2075", "\u2076",
-            "\u2077", "\u2079", "\u2070", nullchar, "\uf6ec", "\uf6f1", "\uf6f3", nullchar,
-            nullchar, "\uf6ed", "\uf6f2", "\uf6eb", nullchar, nullchar, nullchar, nullchar,
-            nullchar, "\uf6ee", "\uf6fb", "\uf6f4", "\uf7af", "\uf6ea", "\u207f", "\uf6ef",
-            "\uf6e2", "\uf6e8", "\uf6f7", "\uf6fc", nullchar, nullchar, nullchar, nullchar,
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 000 - 007
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 010 - 017
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 020 - 027
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 030 - 037
+            "\u0020", "\uf721", "\uf6f8", "\uf7a2", "\uf724", "\uf6e4", "\uf726", "\uf7b4", // 040 - 047
+            "\u207d", "\u207e", "\u2025", "\u2024", "\u002c", "\u002d", "\u002e", "\u2044", // 050 - 057
+            "\uf730", "\uf731", "\uf732", "\uf733", "\uf734", "\uf735", "\uf736", "\uf737", // 060 - 067
+            "\uf738", "\uf739", "\u003a", "\u003b", nullchar, "\uf6de", nullchar, "\uf73f", // 070 - 077
+            nullchar, nullchar, nullchar, nullchar, "\uf7f0", nullchar, nullchar, "\u00bc", // 100 - 107
+            "\u00bd", "\u00be", "\u215b", "\u215c", "\u215d", "\u215e", "\u2153", "\u2154", // 110 - 117
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, "\ufb00", "\ufb01", // 120 - 127
+            "\ufb02", "\ufb03", "\ufb04", "\u208d", nullchar, "\u208e", "\uf6f6", "\uf6e5", // 130 - 137
+            "\uf760", "\uf761", "\uf762", "\uf763", "\uf764", "\uf765", "\uf766", "\uf767", // 140 - 147
+            "\uf768", "\uf769", "\uf76a", "\uf76b", "\uf76c", "\uf76d", "\uf76e", "\uf76f", // 150 - 157
+            "\uf770", "\uf771", "\uf772", "\uf773", "\uf774", "\uf775", "\uf776", "\uf777", // 160 - 167
+            "\uf778", "\uf779", "\uf77a", "\u20a1", "\uf6dc", "\uf6dd", "\uf6fe", nullchar, // 170 - 177
+            nullchar, "\uf6e9", "\uf6e0", nullchar, nullchar, nullchar, nullchar, "\uf7e1", // 200 - 207
+            "\uf7e0", "\uf7e2", "\uf7e4", "\uf7e3", "\uf7e5", "\uf7e7", "\uf7e9", "\uf7e8", // 210 - 217
+            "\uf7ea", "\uf7eb", "\uf7ed", "\uf7ec", "\uf7ee", "\uf7ef", "\uf7f1", "\uf7f3", // 220 - 227
+            "\uf7f2", "\uf7f4", "\uf7f6", "\uf7f5", "\uf7fa", "\uf7f9", "\uf7fb", "\uf7fc", // 230 - 237
+            nullchar, "\u2078", "\u2084", "\u2083", "\u2086", "\u2088", "\u2087", "\uf6fd", // 240 - 247
+            nullchar, "\uf6df", "\u2082", nullchar, "\uf7a8", nullchar, "\uf6f5", "\uf6f0", // 250 - 257
+            "\u2085", nullchar, "\uf6e1", "\uf6e7", "\uf7fd", nullchar, "\uf6e3", nullchar, // 260 - 267
+            nullchar, "\uf7fe", nullchar, "\u2089", "\u2080", "\uf6ff", "\uf7e6", "\uf7f8", // 270 - 277
+            "\uf7bf", "\u2081", "\uf6f9", nullchar, nullchar, nullchar, nullchar, nullchar, // 300 - 307
+            nullchar, "\uf7b8", nullchar, nullchar, nullchar, nullchar, nullchar, "\uf6fa", // 310 - 317
+            "\u2012", "\uf6e6", nullchar, nullchar, nullchar, nullchar, "\uf7a1", nullchar, // 320 - 327
+            "\uf7ff", nullchar, "\u00b9", "\u00b2", "\u00b3", "\u2074", "\u2075", "\u2076", // 330 - 337
+            "\u2077", "\u2079", "\u2070", nullchar, "\uf6ec", "\uf6f1", "\uf6f3", nullchar, // 340 - 347
+            nullchar, "\uf6ed", "\uf6f2", "\uf6eb", nullchar, nullchar, nullchar, nullchar, // 350 - 357
+            nullchar, "\uf6ee", "\uf6fb", "\uf6f4", "\uf7af", "\uf6ea", "\u207f", "\uf6ef", // 360 - 367
+            "\uf6e2", "\uf6e8", "\uf6f7", "\uf6fc", nullchar, nullchar, nullchar, nullchar, // 370 - 377
         };
 
         private static readonly string?[] glyphNames = GetGlyphNameLookup(chars);
--- src/PdfToSvg/Encodings/StandardEncoding.cs
+++ src/PdfToSvg/Encodings/StandardEncoding.cs
@@ -18,38 +18,38 @@ namespace PdfToSvg.Encodings
 
         private static readonly string?[] chars = new[]
         {
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            "\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", "\u2019",
-            "\u0028", "\u0029", "\u002a", "\u002b", "\u002c", "\u002d", "\u002e", "\u002f",
-            "\u0030", "\u0031", "\u0032", "\u0033", "\u0034", "\u0035", "\u0036", "\u0037",
-            "\u0038", "\u0039", "\u003a", "\u003b", "\u003c", "\u003d", "\u003e", "\u003f",
-            "\u0040", "\u0041", "\u0042", "\u0043", "\u0044", "\u0045", "\u0046", "\u0047",
-            "\u0048", "\u0049", "\u004a", "\u004b", "\u004c", "\u004d", "\u004e", "\u004f",
-            "\u0050", "\u0051", "\u0052", "\u0053", "\u0054", "\u0055", "\u0056", "\u0057",
-            "\u0058", "\u0059", "\u005a", "\u005b", "\u005c", "\u005d", "\u005e", "\u005f",
-            "\u2018", "\u0061", "\u0062", "\u0063", "\u0064", "\u0065", "\u0066", "\u0067",
-            "\u0068", "\u0069", "\u006a", "\u006b", "\u006c", "\u006d", "\u006e", "\u006f",
-            "\u0070", "\u0071", "\u0072", "\u0073", "\u0074", "\u0075", "\u0076", "\u0077",
-            "\u0078", "\u0079", "\u007a", "\u007b", "\u007c", "\u007d", "\u007e", nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, "\u00a1", "\u00a2", "\u00a3", "\u2044", "\u00a5", "\u0192", "\u00a7",
-            "\u00a4", "\u0027", "\u201c", "\u00ab", "\u2039", "\u203a", "\ufb01", "\ufb02",
-            nullchar, "\u2013", "\u2020", "\u2021", "\u00b7", nullchar, "\u00b6", "\u2022",
-            "\u201a", "\u201e", "\u201d", "\u00bb", "\u2026", "\u2030", nullchar, "\u00bf",
-            nullchar, "\u0060", "\u00b4", "\u02c6", "\u02dc", "\u00af", "\u02d8", "\u02d9",
-            "\u00a8", nullchar, "\u02da", "\u00b8", nullchar, "\u02dd", "\u02db", "\u02c7",
-            "\u2014", nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar,
-            nullchar, "\u00c6", nullchar, "\u00aa", nullchar, nullchar, nullchar, nullchar,
-            "\u0141", "\u00d8", "\u0152", "\u00ba", nullchar, nullchar, nullchar, nullchar,
-            nullchar, "\u00e6", nullchar, nullchar, nullchar, "\u0131", nullchar, nullchar,
-            "\u0142", "\u00f8", "\u0153", "\u00df", nullchar, nullchar, nullchar, nullchar,
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 000 - 007
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 010 - 017
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 020 - 027
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 030 - 037
+            "\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", "\u2019", // 040 - 047
+            "\u0028", "\u0029", "\u002a", "\u002b", "\u002c", "\u002d", "\u002e", "\u002f", // 050 - 057
+            "\u0030", "\u0031", "\u0032", "\u0033", "\u0034", "\u0035", "\u0036", "\u0037", // 060 - 067
+            "\u0038", "\u0039", "\u003a", "\u003b", "\u003c", "\u003d", "\u003e", "\u003f", // 070 - 077
+            "\u0040", "\u0041", "\u0042", "\u0043", "\u0044", "\u0045", "\u0046", "\u0047", // 100 - 107
+            "\u0048", "\u0049", "\u004a", "\u004b", "\u004c", "\u004d", "\u004e", "\u004f", // 110 - 117
+            "\u0050", "\u0051", "\u0052", "\u0053", "\u0054", "\u0055", "\u0056", "\u0057", // 120 - 127
+            "\u0058", "\u0059", "\u005a", "\u005b", "\u005c", "\u005d", "\u005e", "\u005f", // 130 - 137
+            "\u2018", "\u0061", "\u0062", "\u0063", "\u0064", "\u0065", "\u0066", "\u0067", // 140 - 147
+            "\u0068", "\u0069", "\u006a", "\u006b", "\u006c", "\u006d", "\u006e", "\u006f", // 150 - 157
+            "\u0070", "\u0071", "\u0072", "\u0073", "\u0074", "\u0075", "\u0076", "\u0077", // 160 - 167
+            "\u0078", "\u0079", "\u007a", "\u007b", "\u007c", "\u007d", "\u007e", nullchar, // 170 - 177
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 200 - 207
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 210 - 217
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 220 - 227
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 230 - 237
+            nullchar, "\u00a1", "\u00a2", "\u00a3", "\u2044", "\u00a5", "\u0192", "\u00a7", // 240 - 247
+            "\u00a4", "\u0027", "\u201c", "\u00ab", "\u2039", "\u203a", "\ufb01", "\ufb02", // 250 - 257
+            nullchar, "\u2013", "\u2020", "\u2021", "\u00b7", nullchar, "\u00b6", "\u2022", // 260 - 267
+            "\u201a", "\u201e", "\u201d", "\u00bb", "\u2026", "\u2030", nullchar, "\u00bf", // 270 - 277
+            nullchar, "\u0060", "\u00b4", "\u02c6", "\u02dc", "\u00af", "\u02d8", "\u02d9", // 300 - 307
+            "\u00a8", nullchar, "\u02da", "\u00b8", nullchar, "\u02dd", "\u02db", "\u02c7", // 310 - 317
+            "\u2014", nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 320 - 327
+            nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, nullchar, // 330 - 337
+            nullchar, "\u00c6", nullchar, "\u00aa", nullchar, nullchar, nullchar, nullchar, // 340 - 347
+            "\u0141", "\u00d8", "\u0152", "\u00ba", nullchar, nullchar, nullchar, nullchar, // 350 - 357
+            nullchar, "\u00e6", nullchar, nullchar, nullchar, "\u0131", nullchar, nullchar, // 360 - 367
+            "\u0142", "\u00f8", "\u0153", "\u00df", nullchar, nullchar, nullchar, nullchar, // 370 - 377
         };
 
         private static readonly string?[] glyphNames = GetGlyphNameLookup(chars);

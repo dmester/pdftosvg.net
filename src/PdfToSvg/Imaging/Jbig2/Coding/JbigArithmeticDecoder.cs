7046a1c828c3832a7a712bdb737ae25214245782
Daniel Mester Pirttijärvi <dmester@users.noreply.github.com>
2026-07-15 21:13:52 +0200

    Aligned JbigArithmeticDecoder with the JPEG 2000 variant

--- src/PdfToSvg/Imaging/Jbig2/Coding/JbigArithmeticDecoder.cs
+++ src/PdfToSvg/Imaging/Jbig2/Coding/JbigArithmeticDecoder.cs
@@ -2,6 +2,7 @@
 // https://github.com/dmester/pdftosvg.net
 // Licensed under the MIT License.
 
+using PdfToSvg.Common;
 using PdfToSvg.IO;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,11 @@ using System.Runtime.CompilerServices;
 
 namespace PdfToSvg.Imaging.Jbig2.Coding
 {
+    /// <summary>
+    /// MQ decoder implemented according to "software convention" decoder in ITU-T T.88 Annex G.
+    /// Note that this is essentially a duplicate of <see cref="Jpx.Coding.JpxMqDecoder"/>, to ensure both decoders are
+    /// as self-contained as possible within their namespaces.
+    /// </summary>
     internal class JbigArithmeticDecoder
     {
         private struct QeEntry
@@ -37,8 +43,7 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         private uint a;
 
         private byte[] data;
-        private int offset;
-        private int count;
+        private int dataEndIndex;
 
         private const int MaxFinishedStateCounter = 2;
         private int finishedStateCounter;
@@ -46,68 +51,55 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         static JbigArithmeticDecoder()
         {
             // Table E.1 – Qe values and probability estimation process
-            var qeData = new int[]
-            {
-                0x5601, 1, 1, 1,
-                0x3401, 2, 6, 0,
-                0x1801, 3, 9, 0,
-                0x0AC1, 4, 12, 0,
-                0x0521, 5, 29, 0,
-                0x0221, 38, 33, 0,
-                0x5601, 7, 6, 1,
-                0x5401, 8, 14, 0,
-                0x4801, 9, 14, 0,
-                0x3801, 10, 14, 0,
-                0x3001, 11, 17, 0,
-                0x2401, 12, 18, 0,
-                0x1C01, 13, 20, 0,
-                0x1601, 29, 21, 0,
-                0x5601, 15, 14, 1,
-                0x5401, 16, 14, 0,
-                0x5101, 17, 15, 0,
-                0x4801, 18, 16, 0,
-                0x3801, 19, 17, 0,
-                0x3401, 20, 18, 0,
-                0x3001, 21, 19, 0,
-                0x2801, 22, 19, 0,
-                0x2401, 23, 20, 0,
-                0x2201, 24, 21, 0,
-                0x1C01, 25, 22, 0,
-                0x1801, 26, 23, 0,
-                0x1601, 27, 24, 0,
-                0x1401, 28, 25, 0,
-                0x1201, 29, 26, 0,
-                0x1101, 30, 27, 0,
-                0x0AC1, 31, 28, 0,
-                0x09C1, 32, 29, 0,
-                0x08A1, 33, 30, 0,
-                0x0521, 34, 31, 0,
-                0x0441, 35, 32, 0,
-                0x02A1, 36, 33, 0,
-                0x0221, 37, 34, 0,
-                0x0141, 38, 35, 0,
-                0x0111, 39, 36, 0,
-                0x0085, 40, 37, 0,
-                0x0049, 41, 38, 0,
-                0x0025, 42, 39, 0,
-                0x0015, 43, 40, 0,
-                0x0009, 44, 41, 0,
-                0x0005, 45, 42, 0,
-                0x0001, 45, 43, 0,
-                0x5601, 46, 46, 0,
-            };
-
-            qe = new QeEntry[qeData.Length / 4];
-
-            for (var i = 0; i < qe.Length; i++)
-            {
-                qe[i] = new QeEntry(
-                    value: (uint)qeData[i * 4],
-                    nmps: (byte)qeData[i * 4 + 1],
-                    nlps: (byte)qeData[i * 4 + 2],
-                    switchValue: qeData[i * 4 + 3] == 1
-                    );
-            }
+            qe = [
+                new QeEntry(0x5601, 1, 1, true),
+                new QeEntry(0x3401, 2, 6, false),
+                new QeEntry(0x1801, 3, 9, false),
+                new QeEntry(0x0AC1, 4, 12, false),
+                new QeEntry(0x0521, 5, 29, false),
+                new QeEntry(0x0221, 38, 33, false),
+                new QeEntry(0x5601, 7, 6, true),
+                new QeEntry(0x5401, 8, 14, false),
+                new QeEntry(0x4801, 9, 14, false),
+                new QeEntry(0x3801, 10, 14, false),
+                new QeEntry(0x3001, 11, 17, false),
+                new QeEntry(0x2401, 12, 18, false),
+                new QeEntry(0x1C01, 13, 20, false),
+                new QeEntry(0x1601, 29, 21, false),
+                new QeEntry(0x5601, 15, 14, true),
+                new QeEntry(0x5401, 16, 14, false),
+                new QeEntry(0x5101, 17, 15, false),
+                new QeEntry(0x4801, 18, 16, false),
+                new QeEntry(0x3801, 19, 17, false),
+                new QeEntry(0x3401, 20, 18, false),
+                new QeEntry(0x3001, 21, 19, false),
+                new QeEntry(0x2801, 22, 19, false),
+                new QeEntry(0x2401, 23, 20, false),
+                new QeEntry(0x2201, 24, 21, false),
+                new QeEntry(0x1C01, 25, 22, false),
+                new QeEntry(0x1801, 26, 23, false),
+                new QeEntry(0x1601, 27, 24, false),
+                new QeEntry(0x1401, 28, 25, false),
+                new QeEntry(0x1201, 29, 26, false),
+                new QeEntry(0x1101, 30, 27, false),
+                new QeEntry(0x0AC1, 31, 28, false),
+                new QeEntry(0x09C1, 32, 29, false),
+                new QeEntry(0x08A1, 33, 30, false),
+                new QeEntry(0x0521, 34, 31, false),
+                new QeEntry(0x0441, 35, 32, false),
+                new QeEntry(0x02A1, 36, 33, false),
+                new QeEntry(0x0221, 37, 34, false),
+                new QeEntry(0x0141, 38, 35, false),
+                new QeEntry(0x0111, 39, 36, false),
+                new QeEntry(0x0085, 40, 37, false),
+                new QeEntry(0x0049, 41, 38, false),
+                new QeEntry(0x0025, 42, 39, false),
+                new QeEntry(0x0015, 43, 40, false),
+                new QeEntry(0x0009, 44, 41, false),
+                new QeEntry(0x0005, 45, 42, false),
+                new QeEntry(0x0001, 45, 43, false),
+                new QeEntry(0x5601, 46, 46, false),
+            ];
         }
 
         public JbigArithmeticDecoder(VariableBitReader reader) : this(reader, int.MaxValue)
@@ -126,19 +118,16 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
                 throw new ArgumentException("The reader must be on a byte boundary", nameof(reader));
             }
 
-            reader.GetBuffer(out data, out offset, out count);
+            reader.GetBuffer(out data, out var readerOffset, out var readerCount);
 
-            offset += reader.Cursor.Cursor;
-            count -= reader.Cursor.Cursor;
+            var readerCursor = Math.Min(reader.Cursor.Cursor, readerCount);
+            var remainingCount = readerCount - readerCursor;
+            var decoderCount = MathUtils.Clamp(remainingCount, min: 0, max: maxByteCount);
 
-            if (count > maxByteCount)
+            checked
             {
-                count = maxByteCount;
-            }
-
-            if (count < 2)
-            {
-                throw new ArgumentException("The reader must contain at least two remaining bytes.", nameof(reader));
+                byteCursor = readerOffset + readerCursor;
+                dataEndIndex = byteCursor + decoderCount;
             }
 
             InitDec();
@@ -146,19 +135,24 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
 
         public JbigArithmeticDecoder(byte[] data, int offset, int count)
         {
-            if (offset < 0 || offset + count > data.Length)
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (offset < 0 || offset > data.Length)
             {
                 throw new ArgumentOutOfRangeException(nameof(offset));
             }
 
-            if (count < 2)
+            if (count < 0 || count > data.Length - offset)
             {
-                throw new ArgumentOutOfRangeException(nameof(count), "The arithmetic encoded data must contain at least two bytes.");
+                throw new ArgumentOutOfRangeException(nameof(count));
             }
 
-            this.data = data ?? throw new ArgumentNullException(nameof(data));
-            this.count = count;
-            this.offset = offset;
+            this.data = data;
+            this.dataEndIndex = offset + count;
+            this.byteCursor = offset;
 
             InitDec();
         }
@@ -167,7 +161,7 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         {
             byteOffset += this.byteCursor;
 
-            if (byteOffset - this.offset >= this.count)
+            if (byteOffset >= this.dataEndIndex)
             {
                 return 0xff;
             }
@@ -181,8 +175,14 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         {
             // Figure G.1
 
-            byteCursor = offset;
-            c = ((uint)data[byteCursor] ^ 0xff) << 16;
+            if (byteCursor < dataEndIndex)
+            {
+                c = ((uint)data[byteCursor] ^ 0xff) << 16;
+            }
+            else
+            {
+                c = 0;
+            }
 
             ByteIn();
 
@@ -225,11 +225,10 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         }
 
         [MethodImpl(MethodInliningOptions.AggressiveInlining)]
-        private bool MpsExchange(ref JbigArithmeticContextEntry context)
+        private bool MpsExchange(ref QeEntry qeEntry, ref JbigArithmeticContextEntry context)
         {
             // Figure E.16
 
-            var qeEntry = qe[context.Index];
             bool d;
 
             if (a < qeEntry.Value)
@@ -253,11 +252,10 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         }
 
         [MethodImpl(MethodInliningOptions.AggressiveInlining)]
-        private bool LpsExchange(ref JbigArithmeticContextEntry context)
+        private bool LpsExchange(ref QeEntry qeEntry, ref JbigArithmeticContextEntry context)
         {
             // Figure E.17
 
-            var qeEntry = qe[context.Index];
             bool d;
 
             if (a < qeEntry.Value)
@@ -410,7 +408,7 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
         {
             // Figure G.2
 
-            var qeEntry = qe[context.Current.Index];
+            ref var qeEntry = ref qe[context.Current.Index];
             bool result;
 
             a = a - qeEntry.Value;
@@ -421,7 +419,7 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
             {
                 if ((a & 0x8000) == 0)
                 {
-                    result = MpsExchange(ref context.Current);
+                    result = MpsExchange(ref qeEntry, ref context.Current);
                     Renormd();
                 }
                 else
@@ -432,9 +430,9 @@ namespace PdfToSvg.Imaging.Jbig2.Coding
             else
             {
                 // Chigh = Chigh - A
-                c = ((Chigh - a) << 16) | (c & 0xffffu);
+                c -= a << 16;
 
-                result = LpsExchange(ref context.Current);
+                result = LpsExchange(ref qeEntry, ref context.Current);
                 Renormd();
             }
 
